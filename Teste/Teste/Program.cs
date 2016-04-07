48756cff5c96313065594557816dc16ee1fddd1b
Wellington Schütz <wellington.schu@live.com>
2016-04-07 10:39:19 -0300

    Initial Commit

---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 1;
+            int result = 0;
+            int count = 0;
+
+            if (sum == 0)
+            {
+                if (count == 0)
+                    result = 0;
+            }
+                else result = 1;
+
+            Console.WriteLine("Result: " + result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TestePython/TestePython/TestePython.py
@@ -0,0 +1,9 @@
+sum = 1
+cout = 0
+result = 0
+
+if sum == 0 :
+    if cont == 0 :
+        result = 0
+    else :
+        result = 1
